3358fa47e133b8e702380a873a896e3a226f98a0
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-08-26 17:46:25 +0100

    Add environs vairable to DEBUG

--- config/settings.py
+++ config/settings.py
@@ -27,9 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = env.bool("DEBUG", default=False)
-DEBUG = True
-
+DEBUG = env.bool("DEBUG", default=False)
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 # Application definition
