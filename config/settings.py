85e942d787096abf8e47bd668da99517d3012fa2
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-08-27 08:54:47 +0100

    Add comment to the whitenose storage

--- config/settings.py
+++ config/settings.py
@@ -123,8 +123,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))] # new
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles')) # new
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # new
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
