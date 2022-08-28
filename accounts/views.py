f55920c8e0f02c3c7524b85e1f7879634116d3b0
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-08-28 14:15:05 +0100

    Add signup feature and wire it

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import CreatUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
+# Register user
+def sign_up(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreatUserForm()
+        if request.method == 'POST':
+            form = CreatUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'User ' + '' + user + '' + ' have been created now you can login')
+                return redirect('login')
+    context = {'form':form}
+    return render(request, 'registration/signup.html', context)
