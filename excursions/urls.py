c2206c7d078ab503c4b737a1d4027a609dc8dad6
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-09-04 15:28:50 +0100

    Add excursion_detailsin view and wire it

--- excursions/urls.py
+++ excursions/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.excursion, name="excursions"),
+    path('detail/<int:pk>', views.excursion_details, name="detail"),
 ]
--- excursions/views.py
+++ excursions/views.py
@@ -10,3 +10,13 @@ def excursion(request):
     excursions = Excursions.objects.all()
     context = {'excursions':excursions}
     return render(request, 'excursions/excursions.html', context)
+
+
+
+def excursion_details(request,pk):
+    """
+    Show the excursion details
+    """
+    excursions = Excursions.objects.get(id=pk)
+    context = {'excursions':excursions}
+    return render(request, 'excursions/excursion_details.html', context)
