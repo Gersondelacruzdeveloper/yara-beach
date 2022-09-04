723f4445a25ff9faa1525384087f6d1be26332b1
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-09-04 14:42:17 +0100

    Add extra field of image_alt to Excursions model

--- excursions/models.py
+++ excursions/models.py
@@ -13,6 +13,7 @@ class Excursions(models.Model):
     """
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     title = models.CharField(max_length=201, null=True)
+    image_alt = models.CharField(max_length=201, null=True, blank=True)
     image = models.ImageField(upload_to='excursions/uploads/', null=True)
     description = RichTextUploadingField(null=True, blank=True)
     Price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
