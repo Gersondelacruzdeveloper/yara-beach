b3177bc1ccc4b2ed3a8d8f23b0b72555a1e89ae6
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-09-18 14:49:09 +0100

    Add all rentals in home and and function in view

--- rentals/views.py
+++ rentals/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Rentals
 
 # Create your views here.
 
 def rentals(request):
     return render(request, 'rentals/rentals.html')
+
+# Show all the rentals
+def rentals(request):
+    rental = Rentals.objects.all()
+    context = {'rentals': rental}
+    return render(request, 'rentals/rentals.html', context)
