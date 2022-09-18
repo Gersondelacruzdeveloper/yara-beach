5e47fe3435411c18546af28c15dd141da1230f25
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-09-18 17:04:39 +0100

    Change excursion url name

--- excursions/urls.py
+++ excursions/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.excursion, name="excursions"),
-    path('detail/<int:pk>', views.excursion_details, name="detail"),
+    path('detail/<int:pk>', views.excursion_details, name="excursion_detail"),
     path('add_images/<int:pk>', views.excursion_images, name="add_images"),
     path('form', views.add_excursions, name="form"),
 ]
