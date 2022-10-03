505e94eca35cf68f6c7c1948b585b41ccf0d28e2
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-10-03 23:01:21 +0100

    Modified home function and query excursion, rentals and modified home templates

--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from excursions.models import Excursions
+from rentals.models import Rentals
 
 # Create your views here.
 
+# show excursions and rentals in home page
 def home(request):
-    return render(request, 'home.html')
+    excursions = Excursions.objects.all()[:4]
+    rentals = Rentals.objects.all()[:4]
+    context = {'excursions': excursions, 'rentals':rentals}
+    return render(request, 'home.html', context)
