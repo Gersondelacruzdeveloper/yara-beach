3631b21cfc0b44123088935853fd7f76d3638f8c
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-10-04 21:32:37 +0100

    Add contact page, templates, style and wire it

--- home/views.py
+++ home/views.py
@@ -10,3 +10,24 @@ def home(request):
     rentals = Rentals.objects.all()[:4]
     context = {'excursions': excursions, 'rentals':rentals}
     return render(request, 'home.html', context)
+
+from django.shortcuts import render
+from excursions.models import Excursions
+from rentals.models import Rentals
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.core.mail import EmailMessage
+
+# Create your views here.
+
+# show excursions and rentals in home page
+def home(request):
+    excursions = Excursions.objects.all()[:4]
+    rentals = Rentals.objects.all()[:4]
+    context = {'excursions': excursions, 'rentals':rentals}
+    return render(request, 'home.html', context)
+
+
+# Contact form
+def contact(request):
+    return render(request, 'contact.html')
