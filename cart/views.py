3934f04506783d56d57fb85d1c2743a950adf0c1
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-10-09 13:58:25 +0100

    Added add to card functionality

--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 
+# return the cart page
 def view_cart(request):
-    """return the index page"""
     return  render(request, 'cart/cart.html')
+
+# Add a quantity of the specified product to the shopping cart
+def add_to_cart(request, item_id):
+    excursion_date = request.POST.get('excursion_date')
+    adult_qty = int(request.POST.get('adult_qty'))
+    child_qty = int(request.POST.get('child_qty'))
+    place_pickup = request.POST.get('place_pickup')
+    redirect_url = request.POST.get("redirect_url")
+    cart = request.session.get('cart', {})
+    if item_id in list(cart.keys()):
+         cart['adult_qty'] += adult_qty
+         cart['child_qty'] +=  child_qty
+         cart['excursion_date'] = excursion_date
+         cart['place_pickup'] = place_pickup
+    else:
+         cart['adult_qty'] = adult_qty
+         cart['child_qty'] =  child_qty
+         cart['excursion_date'] = excursion_date
+         cart['place_pickup'] = place_pickup
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
