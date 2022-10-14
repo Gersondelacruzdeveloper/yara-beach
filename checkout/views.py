7c6d9cd96a0536d39850f189db48745863ce4bed
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-10-14 09:32:10 +0100

    Add stripe element

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages
-
 # Create your views here.
 
 # return the cart page
@@ -9,5 +8,9 @@ def checkout(request):
     if not cart:
         messages.error(request, "There's nothing in your cart at the moment")
         return redirect('excursions')
-
-    return render(request, 'checkout/checkout.html')
+    
+    context = {
+        'stripe_public_key': 'pk_test_51LsX2bBFqdL0Ka0uBMY3tRh7VxHDOj8KBkKmhbZLHQnyF9Uobngt07yrulCqWVG1sgffi1VwKcjUPcgRKllhruer00X74q4GDr',
+        'client_secret': 'test client secret',
+    }
+    return render(request, 'checkout/checkout.html', context)
