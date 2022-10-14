2825db13231d66fc2690f58d508b847473c29b5e
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-10-14 11:02:20 +0100

    Add basic stripe functionality

--- checkout/views.py
+++ checkout/views.py
@@ -1,16 +1,31 @@
+from locale import currency
 from django.shortcuts import redirect, render
 from django.contrib import messages
+from cart.contexts import cart_contents
+from django.conf import settings
+import stripe
 # Create your views here.
 
 # return the cart page
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "There's nothing in your cart at the moment")
         return redirect('excursions')
-    
+
+    current_cart = cart_contents(request)
+    total = current_cart['total']
+    stripe_total = round(total * 100)
+    stripe.api_key =stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount = stripe_total,
+        currency = settings.STRIPE_CURRENCY
+    )
+
     context = {
-        'stripe_public_key': 'pk_test_51LsX2bBFqdL0Ka0uBMY3tRh7VxHDOj8KBkKmhbZLHQnyF9Uobngt07yrulCqWVG1sgffi1VwKcjUPcgRKllhruer00X74q4GDr',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
     return render(request, 'checkout/checkout.html', context)
