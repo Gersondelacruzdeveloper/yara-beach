94e04769e94612b6b98937cfd41b6468d0521d64
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-10-17 13:09:15 +0100

    Remove order_total from ExcursionOrder model

--- checkout/models.py
+++ checkout/models.py
@@ -13,7 +13,6 @@ class ExcursionOrder(models.Model):
     full_name = models.CharField(max_length=70, null=False, blank=False)
     image = models.TextField(validators=[URLValidator()], null=True, blank=True)
     price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
-    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, default=0)
     adult_qty =  models.IntegerField(null=False, blank=False)
     child_qty =  models.IntegerField(null=False, blank=False)
     excursion_date = models.DateField(null=False, blank=False)
