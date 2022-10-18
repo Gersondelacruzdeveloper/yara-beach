ff6787bf45f61116a8b9f047e9d6535602322518
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-10-18 20:51:48 +0100

    Add rental bookings for customers

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('contact/', views.contact, name="contact"),
     path('comfirmation/', views.email_comfirmation_page, name="comfirmation"),
+    # excursion bookings
     path('bookings/', views.customer_bookings, name="excursion-bookings"),
+    # rental bookings
+    path('bookings_rental/', views.customer_rental_bookings, name="rental-bookings"),
 ]
--- home/views.py
+++ home/views.py
@@ -7,7 +7,7 @@ from rentals.models import Rentals
 from django.template.loader import render_to_string
 from django.conf import settings
 from django.core.mail import EmailMessage
-from checkout.models import ExcursionOrder
+from checkout.models import ExcursionOrder, RentalOrder
 import datetime
 # Create your views here.
 
@@ -66,24 +66,39 @@ def page_not_found(request, exception):
 def customer_bookings(request):
     # Excursion queries
     user_orders = ExcursionOrder.objects.all().filter(user=request.user)
-    print('user_orders', user_orders)
     # today bookins
     today_excursion_bookings = user_orders.filter(
         excursion_date=datetime.date.today())
-    print('today', today_excursion_bookings)
     # future bookings
     future_excursion_bookings = user_orders.filter(
         excursion_date__gte=datetime.date.today())
-
-    print('future', future_excursion_bookings)
     # pass bookings
     previous_excursion_bookings = user_orders.filter(
         excursion_date__lte=datetime.date.today())
-
-    print('previous', previous_excursion_bookings)
 
     context = {'today_excursion_bookings': today_excursion_bookings,
                'future_excursion_bookings': future_excursion_bookings,
                'previous_excursion_bookings': previous_excursion_bookings}
     return render(request, 'home/customer_booking.html', context)
+
+
+
+# Show  all the customer bookings
+def customer_rental_bookings(request):
+    # rental queries
+    user_orders = RentalOrder.objects.all().filter(user=request.user)
+    # today bookins
+    today_rental_bookings = user_orders.filter(
+        check_in=datetime.date.today())
+    # future bookings
+    future_rental_bookings = user_orders.filter(
+        check_in__gte=datetime.date.today())
+    # pass bookings
+    previous_rental_bookings = user_orders.filter(
+        check_in__lte=datetime.date.today())
+
+    context = {'today_rental_bookings': today_rental_bookings,
+               'future_rental_bookings': future_rental_bookings,
+               'previous_rental_bookings': previous_rental_bookings}
+    return render(request, 'home/customer_rental_booking.html', context)
 
