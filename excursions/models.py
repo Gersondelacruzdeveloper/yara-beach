6496cf8fadf3361776df13d77ab43356a6dcd86a
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-10-19 19:29:58 +0100

    Modified price and description field in excursion and rental

--- excursions/models.py
+++ excursions/models.py
@@ -13,10 +13,10 @@ class Excursions(models.Model):
     )
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     title = models.CharField(max_length=201, null=True)
-    Price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    Price = models.DecimalField(max_digits=7, decimal_places=2, null=False, blank=False, default=0)
     main_image = models.ImageField(upload_to='excursions/uploads/', null=True)
     image_name = models.CharField(max_length=201, null=True, blank=True)
-    description = RichTextUploadingField(null=True, blank=True)
+    description = RichTextUploadingField(null=False, blank=False, default='')
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(choices=CHOICES, default='Inactive', max_length=20)
 
