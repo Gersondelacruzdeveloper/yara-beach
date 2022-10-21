1d26f33e80188a01a247f4cc26aaaf5e4377cc18
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-10-21 01:31:30 +0100

    Delete RentalOrders and myInventions module from checkout

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import ExcursionOrder, RentalOrders, myInventions, accommodationOrder
+from .models import ExcursionOrder, accommodationOrder
 admin.site.register(ExcursionOrder)
-admin.site.register(RentalOrders)
-admin.site.register(myInventions)
 admin.site.register(accommodationOrder)
