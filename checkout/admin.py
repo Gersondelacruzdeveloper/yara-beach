5b00029c1f1b9b72da29efda31a5d795df54ea6f
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-10-21 01:12:55 +0100

    Add accommodationOrder model to checkout app migrations

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import ExcursionOrder, RentalOrders, myInventions
+from .models import ExcursionOrder, RentalOrders, myInventions, accommodationOrder
 admin.site.register(ExcursionOrder)
 admin.site.register(RentalOrders)
 admin.site.register(myInventions)
+admin.site.register(accommodationOrder)
--- checkout/models.py
+++ checkout/models.py
@@ -89,3 +89,43 @@ class myInventions(models.Model):
 
     def __str__(self):
         return self.rental_name
+
+
+
+
+# Contains all orders related to rentals
+class accommodationOrder(models.Model):
+    rental_name = models.CharField(max_length=300, null=False, blank=False)
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE, default='')
+    order_number = models.CharField(max_length=32, null=False, blank=False, editable=False)
+    full_name = models.CharField(max_length=70, null=False, blank=False)
+    image = models.TextField(validators=[URLValidator()], null=True, blank=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    adult_qty =  models.IntegerField(null=False, blank=False)
+    child_qty =  models.IntegerField(null=False, blank=False)
+    check_in = models.DateField(null=False, blank=False)
+    checkout = models.DateField(null=False, blank=False)
+    date_created = models.DateField(auto_now_add=True)
+    customer_email = models.EmailField(null=True, blank=True)
+    cellphone_number = models.CharField(max_length=70, null=True, blank=True)
+    rental_type = models.CharField(max_length=70, null=True, blank=True)
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+    def _generate_rental_order_number(self):
+        """
+        Generate a ramdom, unique order number using UUID for rental
+        """
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """
+        Overide the original save method to set the order number
+        if it has not been set already
+        """
+        if not self.order_number:
+            self.order_number = self._generate_rental_order_number()
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
