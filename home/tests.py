3e9b0b545314cf2e46943e28519df1f6a9e9e0f9
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-10-22 14:07:29 +0100

    Add test for home

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+def test_home_list_view(self):
+    """
+    Test home list view status response
+    and template used.
+    """
+    response = self.client.get("/home/")
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, 'home/home.html')
