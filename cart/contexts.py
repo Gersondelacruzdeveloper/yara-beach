f3af451a51c02fa4446ad5d065947e5ce4f3a22f
Gerson <gdelacruz.developer  @gmail.com>
2023-09-19 11:31:40 -0400

    fix id error

--- cart/contexts.py
+++ cart/contexts.py
@@ -4,8 +4,9 @@ from excursions.models import Excursions
 from rentals.models import Rentals
 from .utils import take_date_from_str, num_of_days
 from datetime import date
-
+from django.http import Http404
 # A content processor, vailable in all templates
+
 def cart_contents(request):
     cart_items = []
     total = Decimal(0.00)
@@ -16,36 +17,40 @@ def cart_contents(request):
     checkout_cart['discount_applied'] = False
 
     for id, value in cart.items():
-        if id:
-         excursion = get_object_or_404(Excursions, pk=id)
-        is_transfer =  excursion.is_transfer
-        total_price_adult = Decimal(value['price']) * int(value['adult_qty'])
-        total_price_children = 0
-        if value['child_qty']:
-            try:
-                total_price_children = Decimal(value['price']) / 2 * int(value['child_qty'])
-            except ValueError:
-                print("An error occur in our system please try again later")
-        total += Decimal(total_price_adult + total_price_children)
-        subTotal = total_price_adult + total_price_children
- 
+        try:
+            excursion = Excursions.objects.get(pk=id)
+            is_transfer = excursion.is_transfer
+            total_price_adult = Decimal(value['price']) * int(value['adult_qty'])
+            total_price_children = 0
+            if value['child_qty']:
+                try:
+                    total_price_children = Decimal(value['price']) / 2 * int(value['child_qty'])
+                except ValueError:
+                    print("An error occurred in our system. Please try again later.")
+            total += Decimal(total_price_adult + total_price_children)
+            subTotal = total_price_adult + total_price_children
 
-        cart_items.append({
-            'item_id':id,
-            'values': value,
-            'excursion':excursion,
-            'is_transfer':is_transfer,
-            'subTotal':subTotal,
-        })
+            cart_items.append({
+                'item_id': id,
+                'values': value,
+                'excursion': excursion,
+                'is_transfer': is_transfer,
+                'subTotal': subTotal,
+            })
+
+        except Excursions.DoesNotExist:
+            # Handle the case where the Excursions object does not exist
+            print(f"Excursions with id {id} does not exist.")
 
     context = {
-            'cart_items':cart_items,
-            'product_count':product_count,
-            'total':total,
+        'cart_items': cart_items,
+        'product_count': product_count,
+        'total': total,
     }
     checkout_cart['total'] = str(total)
     request.session['checkout_cart'] = checkout_cart
     return context
+
 
 
 # A content processor for rental cart
