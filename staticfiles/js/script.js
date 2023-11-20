777e3d181c8583617b496b5e3ae622840bc126b2
Gerson <gdelacruz.developer  @gmail.com>
2023-11-19 21:21:49 -0400

    Add arows to img in detail

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,19 +1,17 @@
 
-// Variables detail page
-let thumbnails = document.getElementsByClassName('thumbnails')
-let activeImages = document.getElementsByClassName('active-image')
-let expandedImg = document.getElementById('expandedImg')
-let arrowLeft = document.getElementById('arrow-left');
-let arrowRight = document.getElementById('arrow-right');
 // Variables edit page
 let upload_img = document.getElementById('id_images')
 
-
 // -----------------------------------------------------------detail page
+let thumbnails = document.getElementsByClassName('thumbnails');
+let activeImages = document.getElementsByClassName('active-image');
+let expandedImg = document.getElementById('expandedImg');
+let arrowLeft = document.getElementById('arrow-left');
+let arrowRight = document.getElementById('arrow-right');
 
-//Add event listener to all thumbnails
+// Add event listener to all thumbnails
 for (let i = 0; i < thumbnails.length; i++) {
-  thumbnails[i].addEventListener('click', switchImages)
+  thumbnails[i].addEventListener('click', switchImages);
 }
 
 /**
@@ -22,24 +20,41 @@ for (let i = 0; i < thumbnails.length; i++) {
 function switchImages() {
   expandedImg.src = this.src;
   if (activeImages.length > 0) {
-    activeImages[0].classList.remove('active-image')
+    activeImages[0].classList.remove('active-image');
   }
-  this.classList.add('active-image')
+  this.classList.add('active-image');
 }
 
-// When click on the left arrow the allphotos div will scroll -50
+// When click on the left arrow, scroll and switch to the previous image
 if (arrowLeft) {
   arrowLeft.addEventListener('click', function () {
-    document.getElementById('allphotos').scrollLeft -= 50
-  })
+    document.getElementById('allphotos').scrollLeft -= 50;
+    switchToPreviousImage();
+  });
 }
 
-// When click on the right arrow the allphotos div will scroll + 50
+// When click on the right arrow, scroll and switch to the next image
 if (arrowRight) {
   arrowRight.addEventListener('click', function () {
-    document.getElementById('allphotos').scrollLeft += 50
-  })
+    document.getElementById('allphotos').scrollLeft += 50;
+    switchToNextImage();
+  });
 }
+
+// Function to switch to the previous image
+function switchToPreviousImage() {
+  let currentIndex = Array.from(thumbnails).indexOf(document.querySelector('.active-image'));
+  let previousIndex = (currentIndex - 1 + thumbnails.length) % thumbnails.length;
+  thumbnails[previousIndex].click();
+}
+
+// Function to switch to the next image
+function switchToNextImage() {
+  let currentIndex = Array.from(thumbnails).indexOf(document.querySelector('.active-image'));
+  let nextIndex = (currentIndex + 1) % thumbnails.length;
+  thumbnails[nextIndex].click();
+}
+
 
 // -------------------------------------------------------------  Edit page
 // Make it posible to add multiple images
